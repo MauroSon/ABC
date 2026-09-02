b6215b758f4785d300fe5903bb2e7846738b74d1
Mauro Ribeiro da Silva <127454440+MauroSon@users.noreply.github.com>
2026-09-02 13:37:17 -0300

    Add function to calculate average of grades

---
+++ teste.py
@@ -0,0 +1,16 @@
+def calcular_media(notas):
+    soma = 0
+
+    for nota in notas
+        soma += nota
+
+    media = soma / len(notas)
+
+    return media
+
+
+notas = [7, 8, 9, 10]
+
+resultado = calcular_media(notas)
+
+print("A média é: " + resultado)
